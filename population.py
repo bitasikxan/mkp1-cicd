f4deba1e8939b2679e2007e9a6c6218b5cc85bed
porokhniavitalii <legenda.repa4@gmail.com>
2025-03-26 15:31:28 +0200

    changed population.py a little bit and added population.txt (3 countries)

--- population.py
+++ population.py
@@ -1,22 +1,20 @@
 def read_population_data(file_path):
     data = []
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r') as file:
             for line in file:
                 name, area, population = line.strip().split(',')
                 data.append((name.strip(), int(area.strip()), int(population.strip())))
     except FileNotFoundError:
-        print(f"File {file_path} not found.")
+        print(f"Файл {file_path} не знайдено.")
         return []
     except ValueError as e:
-        print(f"Error parsing data: {e}")
+        print(f"Помилка при парсингу даних: {e}")
         return []
     return data
 
 def sort_by_area(data):
-    """Sorts population data by area in ascending order."""
-    return sorted(data, key=lambda x: x[1])
+    return sorted(data, key=data[1])
 
 def sort_by_population(data):
-    """Sorts population data by population in ascending order."""
-    return sorted(data, key=lambda x: x[2])
+    return sorted(data, key=data[2])
